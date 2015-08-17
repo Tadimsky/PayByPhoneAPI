5b5bf33b7af4243882c7b497ab8aaecf51feb7a4
Jonno Schmidt <jonno@schmidtfam.us>
2015-08-17 14:06:12 -0700

    Added VehicleManager class

--- PayByPhoneAPI/Vehicle.cs
+++ PayByPhoneAPI/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,54 @@ namespace PayByPhoneAPI
         {
             private string myLicensePlate;
             private VehicleType myType;
+            private string myVehicleID;
+            private VehicleWebData myWebData;
+            private bool isFromWebsite;
+
+            public Vehicle()
+            {
+                myLicensePlate = "";
+                myType = VehicleType.Car;
+                myVehicleID = "0";
+                myWebData = new VehicleWebData();
+                isFromWebsite = false;
+            }
+
+            public static Vehicle Parse(HtmlAgilityPack.HtmlNode htmlData)
+            {
+                var vehicleInput = htmlData.SelectSingleNode(".//input[@type='text']/@value");
+                if (vehicleInput == null)
+                {
+                    // not a vehicle row
+                    return null;
+                }
+                return new Vehicle(htmlData);
+            }
+
+            private Vehicle(HtmlAgilityPack.HtmlNode htmlData)
+            {
+                // parse the html node to get the info out
+
+                var vehicleInput = htmlData.SelectSingleNode(".//input[@type='text']/@value");
+                string licensePlate = vehicleInput?.Attributes["value"].Value;                
+
+                var vehicleType = htmlData.SelectSingleNode(".//select/option[@selected='selected']/@value");
+                string vehicleTypeVal = vehicleType?.Attributes["value"].Value;
+
+                var hiddenData = htmlData.SelectNodes(".//input[@type='hidden']");
+                var hiddenIDInputCol = hiddenData.Where(input => input.Attributes["name"]?.Value.Contains("VehicleUid") == true);
+                var hiddenIDInput = hiddenIDInputCol.First();
+                var hiddenID = hiddenIDInput?.Attributes["value"]?.Value;                
+
+
+                myType = (VehicleType)int.Parse(vehicleTypeVal);
+                myLicensePlate = licensePlate;
+                myVehicleID = hiddenID;
+
+                myWebData = new VehicleWebData(htmlData);
+                isFromWebsite = true;
+            }
+
 
             public string LicensePlate
             {
@@ -22,6 +71,19 @@ namespace PayByPhoneAPI
                 set
                 {
                     myLicensePlate = value;
+                }
+            }
+
+            public string VehicleID
+            {
+                get
+                {
+                    return myVehicleID;
+                }
+
+                set
+                {
+                    myVehicleID = value;
                 }
             }
 
@@ -37,6 +99,47 @@ namespace PayByPhoneAPI
                     myType = value;
                 }
             }
+
+            public NameValueCollection WebData
+            {
+                get
+                {
+                    NameValueCollection nvc = new NameValueCollection();
+                    nvc.Add(myWebData.LicensePlateTextBox, myLicensePlate);
+                    nvc.Add(myWebData.VehicleUIDHiddenField, myVehicleID);
+                    nvc.Add(myWebData.LicensePlateHiddenField, isFromWebsite ? myLicensePlate : "");
+                    nvc.Add(myWebData.VehicleTypeHiddenField, myType.ToString());
+                    nvc.Add(myWebData.VehicleTypeDropDown, myType.ToString());
+                    return nvc;
+                }
+
+            }
+
+            class VehicleWebData
+            {
+                public string LicensePlateTextBox { get; set; }
+                public string VehicleUIDHiddenField{ get; set; }
+                public string LicensePlateHiddenField { get; set; }
+                public string VehicleTypeHiddenField { get; set; }
+                public string VehicleTypeDropDown { get; set; }
+
+                public VehicleWebData()
+                {
+
+                }
+
+                public VehicleWebData(HtmlAgilityPack.HtmlNode vehicleData)
+                {
+                    var allInputs = vehicleData.SelectNodes(".//input");
+                    var select = vehicleData.SelectSingleNode(".//select");
+
+                    LicensePlateTextBox = allInputs.Where(input => input.Attributes["name"]?.Value?.Contains("LicensePlateTextBox") == true).First()?.GetAttributeValue("name", "");
+                    LicensePlateHiddenField = allInputs.Where(input => input.Attributes["name"]?.Value?.Contains("LicensePlateHiddenField") == true).First()?.GetAttributeValue("name", "");
+                    VehicleUIDHiddenField = allInputs.Where(input => input.Attributes["name"]?.Value?.Contains("VehicleUidHiddenField") == true).First()?.GetAttributeValue("name", "");
+                    VehicleTypeHiddenField = allInputs.Where(input => input.Attributes["name"]?.Value?.Contains("VehicleTypeUidHiddenField") == true).First()?.GetAttributeValue("name", "");
+                    VehicleTypeDropDown = select?.GetAttributeValue("name", "");
+                }
+            }
         }
         public enum VehicleType
         {
@@ -44,7 +147,7 @@ namespace PayByPhoneAPI
             Motorcycle = 2, 
             ElectricMotorcycle = 3,
             HeavyGoodsVehicle = 4
-        }
+        }        
     }
     
 }
---
+++ PayByPhoneAPI/VehicleManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayByPhoneAPI.Items;
+
+namespace PayByPhoneAPI
+{
+    class VehicleManager
+    {
+        private PayByPhoneAPI myAPI;
+
+        public List<Vehicle> Vehicles { get; set; }
+
+        public VehicleManager(PayByPhoneAPI api)
+        {
+            myAPI = api;
+            Vehicles = new List<Items.Vehicle>();
+        }
+
+        public bool LoadVehicles()
+        {
+            return true;
+        }
+
+        public bool CreateVehicle(Vehicle vehicle)
+        {
+            return true;
+        }
+
+        public bool UpdateVehicle(Vehicle vehicle)
+        {
+            return true;
+        }
+
+        public bool DeleteVehicle(Vehicle vehicle)
+        {
+            return true;
+        }
+    }
+}
