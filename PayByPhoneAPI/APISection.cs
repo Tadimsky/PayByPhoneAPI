d8bd488a6ffe7f1550e6a770982c99ee5ea2a72e
Jonno Schmidt <jonno@schmidtfam.us>
2015-08-21 16:32:03 -0700

    Added method to API Section to load right page

--- PayByPhoneAPI/APISection.cs
+++ PayByPhoneAPI/APISection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,30 @@ namespace PayByPhoneAPI
             myAPI = api;
         }
 
+        protected async Task<HtmlAgilityPack.HtmlDocument> loadOptions(string optionTarget)
+        {
+            // load the options page
+            await myAPI.CallAPI("OtherOptions.aspx", false);
+
+            // send request to get to card page
+            NameValueCollection info = new NameValueCollection();
+            info.Add("__EVENTTARGET", optionTarget);
+            var doc = await myAPI.CallAPI("OtherOptions.aspx", true, info);
+
+            // return the card info
+            return doc;
+        }
+    }
+
+    namespace Sections
+    {
+        static class Button
+        {
+            public const string PaymentDetails = "ctl00$ContentPlaceHolder1$PaymentDetailsButton";
+            public const string Vehicles = "ctl00$ContentPlaceHolder1$EditVehiclesButton";
+            public const string EmailSettings = "ctl00$ContentPlaceHolder1$TextEmailSettingsButton";
+            public const string SecuritySettings = "yolo";
+            public const string TermsConditions = "yolo";
+        }
     }
 }
