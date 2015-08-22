38ea28c3b464a21e3e7d832334beec22a4047c49
Jonno Schmidt <jonno@schmidtfam.us>
2015-08-21 17:18:37 -0700

    Updated the SecuritySetting details

--- PayByPhoneAPI/SecuritySettings.cs
+++ PayByPhoneAPI/SecuritySettings.cs
@@ -50,28 +50,39 @@ namespace PayByPhoneAPI
 
         class SecuritySetting
         {
-            public string EmailAddress { get; set; }
-            public bool EmailReceipts { get; set; }
-            public bool TextReminders { get; set; }
-            public string Language { get; set; }
+            public string Pin { get; private set; }
+            public bool RememberPin { get; set; }
+            public bool SkipVoicePin{ get; set; }
+
+            private string oldPin;
 
             public SecuritySetting()
             {
-                EmailAddress = "";
-                EmailReceipts = false;
-                TextReminders = false;
-                Language = "1";
+                Pin = "";
+                oldPin = "";
+                RememberPin = false;
+                SkipVoicePin = false;
+            }
+
+            public bool ChangePin(string currentPin, string newPin, string confirmPin)
+            {                
+                if (newPin.Equals(confirmPin))
+                {
+                    oldPin = Pin;
+                    Pin = newPin;
+                    return true;
+                }
+                else
+                {
+                    throw new Exception("Pins do not match.");
+                }                        
             }
 
             public SecuritySetting(HtmlAgilityPack.HtmlNode securityData)
             {
-                // process the data to get the email settings
-                
-                EmailAddress = securityData.SelectSingleNode(String.Format(".//input[@name='{0}']", FormInputNames.EmailSettings.EmailAddress))?.GetAttributeValue("value", "");
-                EmailReceipts = securityData.SelectSingleNode(String.Format(".//input[@name='{0}']", FormInputNames.EmailSettings.EmailRecipientsCheckBox))?.GetAttributeValue("checked", "off") == "checked" ? true : false;
-                TextReminders = securityData.SelectSingleNode(String.Format(".//input[@name='{0}']", FormInputNames.EmailSettings.TextRemindersCheckBox))?.GetAttributeValue("checked", "off") == "checked" ? true : false;
-                var select = securityData.SelectSingleNode(String.Format(".//select[@name='{0}']", FormInputNames.EmailSettings.DropDownPreferredLanguage));
-                Language = select.SelectSingleNode(".//option[@selected='selected']")?.GetAttributeValue("value", "");
+                // process the data to get the security settings                                
+                RememberPin = securityData.SelectSingleNode(String.Format(".//input[@name='{0}']", FormInputNames.SecuritySettings.RememberPin))?.GetAttributeValue("checked", "off") == "checked" ? true : false;
+                SkipVoicePin = securityData.SelectSingleNode(String.Format(".//input[@name='{0}']", FormInputNames.SecuritySettings.VoiceSkipPin))?.GetAttributeValue("checked", "off") == "checked" ? true : false;                
             }
 
             public NameValueCollection WebFormData
@@ -79,23 +90,25 @@ namespace PayByPhoneAPI
                 get
                 {
                     NameValueCollection nvc = new NameValueCollection();
-                    nvc.Add(FormInputNames.EmailSettings.EmailAddress, EmailAddress);
-                    if (EmailReceipts)
+                    nvc.Add(FormInputNames.SecuritySettings.CurrentPin, oldPin);
+                    nvc.Add(FormInputNames.SecuritySettings.NewPin, Pin);
+                    nvc.Add(FormInputNames.SecuritySettings.ConfirmPin, Pin);
+
+                    if (SkipVoicePin)
                     {
-                        nvc.Add(FormInputNames.EmailSettings.EmailRecipientsCheckBox, "on");
+                        nvc.Add(FormInputNames.SecuritySettings.VoiceSkipPin, "on");
                     }
-                    if (TextReminders)
+                    if (RememberPin)
                     {
-                        nvc.Add(FormInputNames.EmailSettings.TextRemindersCheckBox, "on");
+                        nvc.Add(FormInputNames.SecuritySettings.RememberPin, "on");
                     }
-                    nvc.Add(FormInputNames.EmailSettings.DropDownPreferredLanguage, Language);
                     return nvc;
                 }
             }
 
             public override string ToString()
             {
-                return String.Format("Email: {0}\tReceipts: {1}\tReminders: {2}\nLanguage: {3}", EmailAddress, EmailReceipts, TextReminders, Language);
+                return String.Format("Remember Me: {0}\tSkip Voice Pin: {1}", RememberPin, SkipVoicePin);
             }
         }
     }
@@ -109,6 +122,7 @@ namespace PayByPhoneAPI
             public const string ConfirmPin = "ctl00$ContentPlaceHolder1$ConfirmPinTextBox";
             public const string VoiceSkipPin = "ctl00$ContentPlaceHolder1$SkipPinCheckBox";
             public const string RememberPin = "ctl00$ContentPlaceHolder1$RememberPinCheckBox";
+            public const string UpdateButton = "ctl00$ContentPlaceHolder1$NextButton";
             public const string UpdateButtonValue = "update";
         }
     }
